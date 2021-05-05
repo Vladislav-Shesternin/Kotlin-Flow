94ba8ca30708ac71b98f07646ecbd7af7358cb89
Vlad Shesternin <valdon1202@gmail.com>
2021-05-05 11:17:23 +0300

    flow | collect

---
+++ app/src/main/java/com/veldan/kotlin_flow/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.veldan.kotlin_flow
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = this::class.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        CoroutineScope(Dispatchers.Main).launch {
+            initFlow().collect {
+                Log.i(TAG, "collect: $it")
+            }
+        }
+    }
+
+    private fun initFlow() = flow {
+        Log.i(TAG, "Start flow")
+        (1..10).forEach {
+            Log.i(TAG, "flow: $it")
+            emit(it)
+            delay(300)
+        }
+    }
+}
